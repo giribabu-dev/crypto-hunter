0b7ac3d27276ceb3d3024a5364474fcf32981db3
giribabu-a <giribabuannapareddi@gmail.com>
2026-01-23 23:53:53 +0530

    Material UI configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { Button } from "@mui/material";
+
 function App() {
   return (
-    <></>
+    <>
+      <h1>Crypto Currency Tracker</h1>
+      <Button variant="contained">App</Button>
+    </>
   )
 };
 
