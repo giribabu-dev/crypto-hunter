ab55978b6e987d7d30710b2405deec29ee8809dc
giribabu-a <giribabuannapareddi@gmail.com>
2026-01-24 09:03:24 +0530

    Configured Components & Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Button } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import CoinDetails from "./pages/CoinDetails";
 
 function App() {
   return (
     <>
-      <h1>Crypto Currency Tracker</h1>
-      <Button variant="contained">App</Button>
+      <Header />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/coin-details/:id" element={<CoinDetails />} />
+      </Routes>
     </>
   )
 };
---
+++ src/pages/CoinDetails.jsx
@@ -0,0 +1,7 @@
+function CoinDetails(){
+    return (
+        <div>Coin Details</div>
+    )
+};
+
+export default CoinDetails;
